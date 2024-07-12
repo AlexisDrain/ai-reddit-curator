963e19c83eb03c7b3aad11b5d80d182cabb0d514
Alexis Drain <alexisdrain97@gmail.com>
2024-07-11 20:03:07 -0700

    we got access token through crawl typescript

--- website/out/crawl.js
+++ website/out/crawl.js
@@ -1,27 +1,33 @@
 import { readFileSync } from 'fs';
 let client_id = process.env['REDDIT_CLIENTID'];
 let reddit_secret = process.env['REDDIT_SECRET'];
+// if we don't have access to github, grab the env secrets locally.
 if (client_id == undefined) {
-    const file = readFileSync('./_misc/scriptSecret.txt', 'utf-8');
+    const file = readFileSync('./../../_misc/scriptSecret.txt', 'utf-8');
     const regex = /REDDIT_CLIENTID:\s*([\w-]+)/;
     const found = file.match(regex);
-    //console.log (found);
     client_id = found[1];
-    console.log(client_id);
 }
 if (reddit_secret == undefined) {
-    const file = readFileSync('./_misc/scriptSecret.txt', 'utf-8');
+    const file = readFileSync('./../../_misc/scriptSecret.txt', 'utf-8');
     const regex = /REDDIT_SECRET:\s*([\w-]+)/;
     const found = file.match(regex);
     reddit_secret = found[1];
-    console.log(reddit_secret);
 }
 export async function get_headers_with_access_token() {
-    const credentials = btoa(`${client_id}:${reddit_secret}`);
+    //const credentials = btoa(`${client_id}:${reddit_secret}`);
+    const auth = Buffer.from(`${client_id}:${reddit_secret}`).toString('base64');
+    const body = new URLSearchParams({
+        'grant_type': 'client_credentials'
+    });
     try {
         const response = await fetch("https://www.reddit.com/api/v1/access_token", {
+            method: "POST",
+            body: body.toString(),
             headers: {
-                'Authorization': `Basic ${credentials}`
+                'User-Agent': "AIRedditCurator/1.0 by my_tummy_hurts",
+                'Authorization': `Basic ${auth}`,
+                'Content-Type': 'application/x-www-form-urlencoded'
             }
         });
         const data = await response.json();
@@ -34,9 +40,7 @@ export async function get_headers_with_access_token() {
 }
 ;
 export async function get_posts(limit = 10, subredditName = "all", sortByTop = false, fimeFilter = "all") {
-    console.log("Get_posts");
     const headers = await get_headers_with_access_token();
     console.log(headers);
-    console.log(typeof (headers));
 }
 ;
